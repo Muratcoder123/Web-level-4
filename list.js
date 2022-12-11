73237a1f74284333e0744dfe46fe8e6a3cdef670
Muratcoder123 <113483121+Muratcoder123@users.noreply.github.com>
2022-12-11 17:29:39 -0500

    Steps in class

--- list.js
+++ list.js
@@ -1,5 +1,44 @@
 $(document).ready(function() {
-  $.get("http://universities.hipolabs.com/search?country=Canada", function(data, status) {
+    $("#collapseButton").html("Universities in" + selectedOption);
+    var newUrl = "http://universities.hipolabs.com/search?country=" + selectedOption
+    $.get(newUrl,function(response, status) {
+        if(status == "success") {
+            $("#universityList").html("");
+            for (let index = 0; index < response.length; index++) {
+            $("#universityList").append(
+                `<a target="_blank" href="${response[index].web_pages[0]}"
+                 class="list-group-item list-group-item-action">${response[index].name}</a>`);
+            
+            }
+        }
+    });
+
+  $("#target").change(function(){
+
+    var selectedOption = $('#target').find(":selected").val();
+    var selectedOption = $('#target').find(":selected").text();
+    $("#collapseButton").html("Universities in" + selectedOption);
+    var newUrl = "http://universities.hipolabs.com/search?country=" + selectedOption
+    $.get(newUrl,function(response, status) {
+        if(status == "success") {
+            $("#universityList").html("");
+            for (let index = 0; index < response.length; index++) {
+            $("#universityList").append(
+                `<a target="_blank" href="${response[index].web_pages[0]}"
+                 class="list-group-item list-group-item-action">${response[index].name}</a>`);
+            
+            }
+        }
+    });
+  });
+
+  $("#target").change(function(e){
+    var selectedOption = $("option:selected", this);
+    var valueSelected = this.value;
+    $("#collapsedButton").html(Universities)
+  });
+
+   $.get("http://universities.hipolabs.com/search?country=&quot;" + selectedCountryValue , function(response, status) {
       if (status == "success" && data != null && data != undefined) {
           for (let index = 0; index < data.length; index++) {
               $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
@@ -9,4 +48,6 @@ $(document).ready(function() {
           alert("Can't load the Universities")
       }
   });
+
+
 });
